4cc528d2673a9861c0c8e0b7542c8259f5111ee7
notrich22 <notrich22@mail.ru>
2021-02-05 19:36:13 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication11.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string.h>
+using namespace std;
+string word = "";
+void upper() {
+
+}
+void lower() {
+
+}
+void lesenka() {
+
+}
+int main()
+{
+    
+	int ch;
+    cout << "Enter word: ";
+    cin >> word;
+	cout << "Choose:\n1. To upper case\n2. To lower case\n3. Lesenka)))";
+	cin >> ch;
+	switch (ch)
+	{
+	case 1: upper();
+	case 2: lower();
+	case 3: lesenka();
+	default:
+		main();
+	}
+}
